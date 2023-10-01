a176b122085f77d0f6225ace9d388103ab0d266e
GhostMobin <s1810677110@ru.ac.bd>
2023-10-02 00:45:16 +0600

    file setup 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import { Root } from "postcss";
+import Hero from "./components/Hero";
+import Header from "./components/Header";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    // element: <Header></Header>,
+    element: <div>Hola</div>,
+    children: [
+      {
+        path: "/",
+        element: <div>hello</div>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
